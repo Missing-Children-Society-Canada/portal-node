bc9c3cdebf9be9a2647a4692f76d96b4b6a5d1a3
Nastassia <nastassia.rashid@gmail.com>
2017-05-20 21:22:16 -0700

    fixed mixed up variables

--- controllers/profilesController.js
+++ controllers/profilesController.js
@@ -3,13 +3,12 @@ var config = require('../config');
 var ProfileApi = require('../apis/profiles');
 
 exports.list = function(req, res){
-  new ProfileApi.getList(config.docDB).then(function(profiles) {
+  new ProfileApi(config.docDB).getList().then(function(profiles) {
     res.json(profiles);
   });
 };
 
 exports.show = function(req, res) {
-  var user = new User(req);
   new ProfileApi(config.docDB).get(req.params.id).then((profile) => {
     res.render('profiles/show', {
       user: req.user,
