5bcbc66b7ab084b00c89303bf27f7d01daaa0a5d
Jef King <jefkin@microsoft.com>
2017-05-25 16:56:54 -0700

    whitespace

--- apis/token.js
+++ apis/token.js
@@ -3,70 +3,63 @@ var DocumentDBClient = require('documentdb').DocumentClient;
 var http = require('http');
 var request = require('request');
 var config = require('../config');
-var Token = function() {
+var Token = function () {
 }
 
-Token.prototype.send = function(userid, email) 
-{
+Token.prototype.send = function (userid, email) {
     return new Promise((resolve, reject) => {
         var r;
         request.post(
-        config.notifyPoliceUrl,
-        { 
-            json:{
-                'email': email,
-                'userid': userid
-            }
-        },
-        function (error, response, body) {
-            if (!error && (response.statusCode == 200 || response.statusCode == 201)) 
+            config.notifyPoliceUrl,
             {
-                // Does this send a success response back?
-                resolve(response);
+                json: {
+                    'email': email,
+                    'userid': userid
+                }
+            },
+            function (error, response, body) {
+                if (!error && (response.statusCode == 200 || response.statusCode == 201)) {
+                    // Does this send a success response back?
+                    resolve(response);
+                }
+                else {
+                    // Does this send a failed response back?
+                    reject(response);
+                }
             }
-            else 
-            {
-                // Does this send a failed response back?
-                reject(response);
-            }
-        }
-    );
-    resolve();
-});
+        );
+        resolve();
+    });
 }
 
-Token.prototype.verify = function(id, token) 
-{
+Token.prototype.verify = function (id, token) {
     return new Promise((resolve, reject) => {
         request.post(
-        config.validateTokenUrl,
-        { 
-            json:{
-                'userid': id,
-                'token': token
-            }
-        },
-        function (error, response, body) {
-            if (!error && (response.statusCode == 200 || response.statusCode == 201)) {
-                // Body should be "true" if token is valid
-                var body = JSON.parse(response.body);
-                if (body ===  true)
-                {
-                    resolve(response.body);
+            config.validateTokenUrl,
+            {
+                json: {
+                    'userid': id,
+                    'token': token
                 }
-                else
-                {
+            },
+            function (error, response, body) {
+                if (!error && (response.statusCode == 200 || response.statusCode == 201)) {
+                    // Body should be "true" if token is valid
+                    var body = JSON.parse(response.body);
+                    if (body === true) {
+                        resolve(response.body);
+                    }
+                    else {
+                        reject(response.body);
+                    }
+                }
+                else {
                     reject(response.body);
                 }
+
             }
-            else
-            {
-                reject(response.body);
-            }
-            
-        }
-    );
-});
+        );
+    });
 }
 
 module.exports = Token;
