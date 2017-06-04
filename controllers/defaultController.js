e6e227bf0efc81fe465eecc428077ded4eedd603
Marc Gagne <marc.gagne@hotmail.com>
2017-06-04 18:29:15 -0400

    Removed console logging

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -4,10 +4,6 @@ var config = require('../config');
 var ProfileApi = require('../apis/profiles');
 
 exports.show = function (req, res) {
-  console.log("-------------------------------------------------");
-  console.log(JSON.stringify(req.user, null, 4));
-  console.log("-------------------------------------------------");
-
   new ProfileApi(config.docDB).get(req.params.id).then((profile) => {
     res.render('profiles/show', {
       user: req.user,
